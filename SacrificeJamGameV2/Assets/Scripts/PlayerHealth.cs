5c03e3e929211354f98a209aa5c3269377e89a6f
ryan-shirey <ryan.shirey@protonmail.com>
2022-07-09 13:23:05 -0700

    test
    test

--- SacrificeJamGameV2/Assets/Scripts/PlayerHealth.cs
+++ SacrificeJamGameV2/Assets/Scripts/PlayerHealth.cs
@@ -6,15 +6,14 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField]
     private int startHealth = 50;
-
+    private Rigidbody2D rb
     private int playerHealth;
-    public Vector2 location;
     public List<Vector2> drops;
     public GameObject oilSplotOBJ;
     // Start is called before the first frame update
     void Start()
     {
-
+        drops.append(rb.location)
     }
 
     // Update is called once per frame
