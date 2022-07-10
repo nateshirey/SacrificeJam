b577dfe3f03f4c87660271cf6003dffc7bfe8f8f
Nate Shirey <nateshirey6@gmail.com>
2022-07-09 23:50:55 -0600

    animation stuff

---
+++ SacrificeJamGameV2/Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+
+    private PlayerCharacterController characterController;
+    private Vector2 movementInput;
+    public float tan;
+    private bool idle = false;
+
+    private void Awake()
+    {
+        characterController = this.GetComponent<PlayerCharacterController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput = characterController.inputMoveValue;
+
+        idle = false;
+        if(characterController.inputMoveValue.sqrMagnitude <= 0.3f)
+        {
+            idle = true;
+        }
+        else
+        {
+            tan = Mathf.Atan2(movementInput.y, movementInput.x) * Mathf.Rad2Deg;
+            tan /= 45;
+        }
+
+        anim.SetFloat("Blend", tan);
+        anim.SetBool("Idle", idle);
+    }
+}
